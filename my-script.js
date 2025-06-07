ee7cc90fa02285b9a5975e30ee0359563ac9a53e
Coleman Grustas <chgrustas@gmail.com>
2025-06-07 16:20:41 -0400

    Refactor etch-a-sketch
    Refactor grid creation, hover effects, and styling.
    Need to break this into smaller commits in the
    future.

--- my-script.js
+++ my-script.js
@@ -1,19 +1,36 @@
-const GRID_SIZE = 16;
-const GRID_LENGTH = 960; // in pixels
-const container = document.querySelector("#container");
 
-for (let i = 0; i < GRID_SIZE; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 0; j < GRID_SIZE; j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.addEventListener('mouseenter', () => {
-            cell.style.backgroundColor = "lightblue";
-        });
-        row.appendChild(cell);
+createGrid();
+addTrail();
+
+// creates an "<squares> by <squares>" grid
+function createGrid(squares = 16) {
+    const GRID_LENGTH = 960; // pixels
+    const container = document.querySelector("#grid");
+    for (let i = 0; i < squares; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < squares; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
     }
 }
-console.log(container);
 
+// adds a "hover" effect to each cell so that the grid divs change color 
+// when your mouse passes over them, leaving a "pixelated" trail 
+function addTrail() {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", () => {
+            cell.style.backgroundColor = "lightblue";
+        });
+    });
+}
+
+// const button = document.querySelector("button");
+// button.addEventListener("click", () => {
+//     const squares = prompt("How many squares per side?");
+    
+// });
